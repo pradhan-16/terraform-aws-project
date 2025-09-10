5a1078513668607e9062da328850deeb27a869b8
piyush <piyush@piyush-Lenovo-IdeaPad-S340-14IWLsharmapiyush.gmail.com>
2025-09-10 19:06:02 +0530

    Updated files  )

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,9 @@
+# ---------------------
 # Fetch Ubuntu AMI (22.04 LTS)
+# ---------------------
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["099720109477"]  # Canonical
 
   filter {
     name   = "name"
@@ -9,11 +11,16 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# ---------------------
 # EC2 Instance with Website
+# ---------------------
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public.id
+  
+  # Deploy EC2 in first public subnet (ap-south-1a)
+  subnet_id              = aws_subnet.public_a.id
+
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   user_data = <<-EOF
@@ -30,3 +37,27 @@ resource "aws_instance" "web" {
     Environment = var.env_name
   }
 }
+
+# ---------------------
+# Optional: You can add a second EC2 in other public subnet (ap-south-1b) if needed for high availability
+# ---------------------
+# resource "aws_instance" "web_b" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+#   subnet_id              = aws_subnet.public_b.id
+#   vpc_security_group_ids = [aws_security_group.sg.id]
+#
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               apt update -y
+#               apt install -y apache2
+#               echo "<h1>Welcome to ${var.env_name} Website - Secondary 🚀</h1>" > /var/www/html/index.html
+#               systemctl enable apache2
+#               systemctl start apache2
+#               EOF
+#
+#   tags = {
+#     Name        = "${var.env_name}-ec2-b"
+#     Environment = var.env_name
+#   }
+# }
--- output.tf
+++ output.tf
@@ -12,15 +12,60 @@
 
 
 
+# output "website_url" {
+#   value = "http://${aws_instance.web.public_ip}"
+# }
+
+# output "rds_endpoint" {
+#   value = aws_db_instance.mydb.endpoint
+# }
+
+# output "vpc_id" {
+#   value = aws_vpc.main.id
+# }
+
+
+
+
+
+# ---------------------
+# EC2 Website URL
+# ---------------------
 output "website_url" {
-  value = "http://${aws_instance.web.public_ip}"
+  description = "Public URL of the EC2 website"
+  value       = "http://${aws_instance.web.public_ip}"
 }
 
+# ---------------------
+# RDS Endpoint
+# ---------------------
 output "rds_endpoint" {
-  value = aws_db_instance.mydb.endpoint
+  description = "RDS DB Endpoint"
+  value       = aws_db_instance.mydb.endpoint
 }
 
+# ---------------------
+# VPC ID (Optional, for reference)
+# ---------------------
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "VPC ID"
+  value       = aws_vpc.main.id
 }
+
+# ---------------------
+# Public Subnets (Optional)
+# ---------------------
+output "public_subnets" {
+  description = "List of Public Subnet IDs"
+  value       = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+}
+
+# ---------------------
+# Private Subnets (Optional)
+# ---------------------
+output "private_subnets" {
+  description = "List of Private Subnet IDs (RDS)"
+  value       = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+}
+
 
