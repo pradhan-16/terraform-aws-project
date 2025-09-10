e7dceabc40f56ca6133239409b0ecc8a1e060e87
piyush <piyush@piyush-Lenovo-IdeaPad-S340-14IWLsharmapiyush.gmail.com>
2025-09-10 19:28:59 +0530

    Updated files  )

--- vpc.tf
+++ vpc.tf
@@ -119,6 +119,13 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  from_port   = 3306
+  to_port     = 3306
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]  # Testing purpose
+}
+
   egress {
     from_port   = 0
     to_port     = 0
